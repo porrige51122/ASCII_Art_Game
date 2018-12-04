a7941e02100a77f337a5ea9ed87cf1a14b2a7019
Aidan Smithers <smithersa@sionschool.org.uk>
2018-12-04 12:00:11 +0000

    Update Question.java

--- Question.java
+++ Question.java
@@ -11,6 +11,10 @@ public class Question {
 		f = new File(path);
 		char[] chars = f.getName().toCharArray();
 		answer = f.getName();
+		char[] characterCount = answer.toCharArray();
+		char[] output = new char[characterCount.length-4];
+		answer.getChars(0, characterCount.length-4, answer, 0);
+		return chars1;
 	}
 
 	public String getAnswer() {
@@ -18,7 +22,7 @@ public class Question {
 	}
 
 	public boolean isAnswerCorrect(String answer) {
-		return this.answer.equals(answer + ".jpg") || this.answer.equals(answer + ".png");
+		return this.answer.equals(answer);
 	}
 
 	public void print(int diff) throws IOException {
